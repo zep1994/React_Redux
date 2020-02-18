eb0affc390762cc657dcafc98b12dd5f8f6e286f
Thomas Matlock <snaxbags@protonmail.com>
2020-02-18 16:09:51 -0600

    Add testing for immutable sets

---
+++ test/core_spec.js
@@ -0,0 +1,19 @@
+import { List, Map } from 'immutable'
+import { expect } from 'chai'
+
+import { setEntries } from '../src/core'
+
+describe('application logic', () => {
+
+    describe('setEntries', () => {
+
+        it ('adds the entries to the state', () => {
+            const state = Map()
+            const entries = ['Trainspotting', '28 Days Later'];
+            const nextState = setEntries(state, entries)
+            expext(nextState).to.equal(Map({
+                entries: List.of('Trainspotting', '28 Days Later')
+            }))
+        })
+    })
+})
